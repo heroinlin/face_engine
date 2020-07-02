522df9a2d428a9a312c916e09a0ecc341d498d4f
heroinlj <heroinlj@gmail.com>
2020-07-02 17:06:54 +0800

    更新人脸检测子仓库，新增视频人脸检测应用

---
+++ applications/face_detection/video_face_detection.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+import argparse
+import glob
+import os
+import sys
+import time
+import cv2
+working_root = os.path.split(os.path.realpath(__file__))[0]
+sys.path.append(os.path.dirname(os.path.dirname(working_root)))
+from modules.object_detection import (OnnxObjectDetector, PyObjectDetector, draw_detection_rects)
+
+
+class VideoObjectDetection(object):
+    """
+    视频目标检测
+    detector  目标检测接口, 默认使用python端onnx接口
+    """
+
+    def __init__(self, detector=None):
+        super(VideoObjectDetection).__init__()
+        self.decay_time = 0
+        self.auto_play_flag = False
+        self.time_printing = True
+        self.detector = detector
+        self.init()
+
+    def init(self):
+        if self.detector is None:
+            # self.detector = PyObjectDetector()
+            self.detector = OnnxObjectDetector()
+
+    def detect(self, video):
+        """
+        对单个视频的目标检测
+        Parameters
+        ----------
+        video      cv2的VideoCapture()类
+
+        Returns
+        -------
+            无
+        """
+        frame_num = 0
+        while True:
+            _, frame = video.read()
+            if frame is None:
+                break
+            frame_num += 1
+            start_time = time.time()
+            if frame_num % 1 == 0:
+                # bounding_boxes = [[<score>,<box>],[<score>,<box>],...],  维度为n * 5
+                bounding_boxes = self.detector.detect(frame)
+                # print(bounding_boxes)
+            else:
+                bounding_boxes = []
+            draw_detection_rects(frame, bounding_boxes)
+            end_time = time.time()
+            if self.time_printing:
+                print("predict time is {:f}".format(end_time - start_time))
+                self.time_printing = False
+            if "win" in sys.platform:
+                cv2.imshow("object_detection", frame)
+                key = cv2.waitKey(self.decay_time)
+                if key == 32:
+                    self.auto_play_flag = not self.auto_play_flag
+                    self.decay_time = 1 if self.auto_play_flag else 0
+                if key == 27:
+                    break
+
+    def destroy(self):
+        self.detector.destroy()
+
+
+def object_detection_for_videos_folder_test(video_folder_path: str,
+                                            checkpoint_file_path: str = None,
+                                            mean: list = None,
+                                            stddev: list = None):
+    """
+        视频文件夹目标检测
+        Parameters
+        ----------
+        test_folder_path 视频文件夹地址
+        checkpoint_file_path 目标检测模型地址
+        mean 目标检测模型训练使用的图像均值
+        stddev 目标检测模型训练使用的图像标准差
+        Returns
+        -------
+            None
+    """
+    detector = None
+    if checkpoint_file_path is not None:
+        detector = OnnxObjectDetector(checkpoint_file_path)
+    video = cv2.VideoCapture()
+    video_object_detection = VideoObjectDetection(detector=detector)
+    for video_index, video_path in enumerate(
+            sorted(glob.glob("{}/*.mp4".format(video_folder_path)))):
+        print(video_index, os.path.basename(video_path))
+        if not video.open(video_path):
+            print("can not open the video: ", video_path)
+            return
+        video_object_detection.detect(video)
+    video.release()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v',
+                        '--video_folder_path',
+                        type=str,
+                        help='video folder path',
+                        default=os.path.join(os.path.dirname(os.path.dirname(working_root)),
+                                             "data/video_data/videos"))
+    parser.add_argument('-m',
+                        '--model',
+                        type=str,
+                        help='object detection checkpoint file path',
+                        default=None)
+    parser.add_argument('--mean',
+                        type=float,
+                        nargs='+',
+                        help='train image mean value',
+                        default=None)
+    parser.add_argument('--stddev',
+                        type=float,
+                        nargs='+',
+                        help='train image stddev value',
+                        default=None)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    print(args)
+    if args.mean is not None:
+        assert type(args.mean) == list and len(args.mean) == 3
+    if args.stddev is not None:
+        assert type(args.stddev) == list and len(args.stddev) == 3
+    object_detection_for_videos_folder_test(args.video_folder_path, args.model,
+                                            args.mean, args.stddev)
+
+
+if __name__ == '__main__':
+    main()
--- modules/object_detection.py
+++ modules/object_detection.py
@@ -7,8 +7,10 @@ import sys
 import cv2
 import numpy as np
 
+working_root = os.path.split(os.path.realpath(__file__))[0]
+sys.path.append(os.path.dirname(working_root))
 from thirdparty.face_detect_inference import \
-    FaceDetector_mtcnn, FaceDetector_ssd
+    FaceDetector_mtcnn, FaceDetector_ssd, FaceDetector_onnx
 
 
 class IObjectDetector(metaclass=abc.ABCMeta):
@@ -76,30 +78,52 @@ class PyObjectDetector(IObjectDetector):
         pass
 
 
-def draw_detection_rects(image: np.ndarray,
-                         detection_rects: list,
-                         color=(0, 255, 0)):
-    for rect in detection_rects:
-        cv2.rectangle(
-            image,
-            (int(rect[1] * image.shape[1]), int(rect[2] * image.shape[0])),
-            (int(rect[3] * image.shape[1]), int(rect[4] * image.shape[0])),
-            color,
-            thickness=2)
-        cv2.putText(
-            image, f"{rect[0]:.03f}",
-            (int(rect[1] * image.shape[1]), int(rect[2] * image.shape[0])), 1,
-            1, (255, 0, 255))
+class OnnxObjectDetector(IObjectDetector):
+    def __init__(self, checkpoint_file_path=None):
+        self.checkpoint_file_path = checkpoint_file_path
+        self.init_interface()
+
+    def init_interface(self):
+        self.detector = FaceDetector_onnx(
+            onnx_file_path=self.checkpoint_file_path)
+
+    def detect(self, image: np.ndarray) -> list:
+        bounding_boxes = self.detector.detect(image)
+        return bounding_boxes
+
+    def set_config(self, key, value):
+        return super().set_config(key, value) 
+
+    def destroy(self):
+        pass
+
+
+def draw_detection_rects(image: np.ndarray, detection_rects: np.ndarray, color=(0, 255, 0), method=1):
+    if not isinstance(detection_rects, np.ndarray):
+        detection_rects = np.array(detection_rects)
+    if method:
+        width = image.shape[1]
+        height = image.shape[0]
+    else:
+        width = 1.0
+        height = 1.0
+    for index in range(detection_rects.shape[0]):
+        cv2.rectangle(image,
+                      (int(detection_rects[index, 0] * width), int(detection_rects[index, 1] * height)),
+                      (int(detection_rects[index, 2] * width), int(detection_rects[index, 3] * height)),
+                      color,
+                      thickness=2)
+        if detection_rects.shape[1] == 5:
+            cv2.putText(image, f"{detection_rects[index, 4]:.03f}",
+                        (int(detection_rects[index, 0] * width), int(detection_rects[index, 1] * height)),
+                        1, 1, (255, 0, 255))
 
 
 def main():
-    working_root = os.path.split(os.path.realpath(__file__))[0]
-    os.chdir(os.path.dirname(working_root))
-    sys.path.append(os.path.dirname(working_root))
-
     detector = PyObjectDetector()
     detector.set_config("detect_threshold", 0.8)
-    image = imread(r"./data/image_data/images/test.jpg")
+    image_path = os.path.join(os.path.dirname(working_root), r"./data/image_data/images/00000_1.jpg")
+    image = cv2.imread(image_path)
     rects = detector.detect(image)
     print(rects)
     draw_detection_rects(image, rects)
